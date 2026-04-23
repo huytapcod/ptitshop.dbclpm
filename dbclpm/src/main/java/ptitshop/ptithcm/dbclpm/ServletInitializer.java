ee2f75aaed58602b51d7eceb21cf2149fb15e038
huytapcod <banvanhuy8959@gmail.com>
2026-04-23 22:20:24 +0700

    fist commit

---
+++ dbclpm/src/main/java/ptitshop/ptithcm/dbclpm/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ptitshop.ptithcm.dbclpm;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DbclpmApplication.class);
+	}
+
+}
---
+++ dbclpm/src/test/java/ptitshop/ptithcm/dbclpm/DbclpmApplicationTests.java
@@ -0,0 +1,13 @@
+package ptitshop.ptithcm.dbclpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbclpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
